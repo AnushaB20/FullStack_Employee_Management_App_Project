883f96aeb882d8e36a609c0873d848795655f40a
Anusha B <anushakbgowda@gmail.com>
2026-06-10 13:12:10 +0530

    Employee Management project

---
+++ FullStackEmployeeManagementAppProject/src/main/java/com/project/FullStackEmployeeManagementAppProjectApplication.java
@@ -0,0 +1,13 @@
+package com.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullStackEmployeeManagementAppProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullStackEmployeeManagementAppProjectApplication.class, args);
+	}
+
+}
---
+++ FullStackEmployeeManagementAppProject/src/main/java/com/project/config/MyUserDetailsService.java
@@ -0,0 +1,32 @@
+package com.project.config;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.project.entity.User;
+import com.project.repository.UserRepository;
+
+@Service
+public class MyUserDetailsService {
+
+	private final UserRepository userRepository;
+	
+	public MyUserDetailsService(UserRepository userRepository)
+	{
+		this.userRepository = userRepository;
+	}
+	
+	 public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+	        User user = userRepository.findByUsername(username);
+	        System.out.println("security usernam "+ username);
+	        
+	        if(user == null)
+	        {
+	        	throw new UsernameNotFoundException("This user doent not exist in the DB");
+	        }
+	       System.out.println("User from DB "+user);
+	       
+	       return new UserPrinciple(user);
+	    }
+}
